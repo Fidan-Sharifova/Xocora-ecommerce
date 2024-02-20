f499af704a9b1323fe8fabf1179169bf1aa904d9
Fidan <tu7s06nxd@code.edu.az>
2024-02-21 02:51:44 +0400

    .

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-const express= require("express")
-const app= express()
-const cors= require("cors")
-const mongoose=require("mongoose")
-const router= require("./router/backend.routes")
-const ROUTER = require("./router/user.routes")
-app.use(express.json())
-app.use(cors())
-app.use("/xocora/products", router)
-app.use("/users", ROUTER)
-mongoose.connect("mongodb+srv://Fidan_Sharifova:fidan_2004@cluster0.zlx6suq.mongodb.net/").then(res=>{
-    console.log("connected to mongoose")
-})
-app.listen(1212, ()=>{
-    console.log("app running on 1212")
-})
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const router = require("./router/backend.routes");
+const ROUTER = require("./router/user.routes");
+app.use(express.json());
+app.use(cors());
+app.use("/xocora/products", router);
+app.use("/users", ROUTER);
+mongoose
+  .connect(
+    "mongodb+srv://Fidan_Sharifova:fidan_2004@cluster0.zlx6suq.mongodb.net/"
+  )
+  .then((res) => {
+    console.log("connected to mongoose");
+  });
+app.listen(1212, () => {
+  console.log("app running on 1212");
+});
